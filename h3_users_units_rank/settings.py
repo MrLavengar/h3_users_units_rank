3742c4a0badc1ba97f3c961cf88f6e20111af8b2
Bartosz Piekarz <MrLavengar@gmail.com>
2019-09-03 16:19:08 +0200

    add database configuration

--- h3_users_units_rank/settings.py
+++ h3_users_units_rank/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'h3_users_units_rank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': 'localhost',
+        'NAME': 'h3_creatures_rank',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
     }
 }
 
